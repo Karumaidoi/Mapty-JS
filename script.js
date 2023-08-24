3ecffe6393a2d779c4f54861e604a7a0f0fc9cdb
Karumaidoi <mainahmwangi12@gmail.com>
2023-08-24 21:08:50 +0300

    added pop up content

--- script.js
+++ script.js
@@ -22,6 +22,31 @@ if (navigator.geolocation) {
 
       const url = `https://www.google.com/maps/@${latitude},${longitude},15.08z?entry=ttu`;
       console.log(url);
+
+      const map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              maxHeight: 250,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function error() {
       console.log('Error');
